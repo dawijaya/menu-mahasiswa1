60a72024bbb899b854add6b237297a1ed895b1bf
dawijaya <113819824+dawijaya@users.noreply.github.com>
2023-04-09 21:44:59 +0700

    Add files via upload

---
+++ Chat.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('keluhan-form');
+const kategori = document.getElementById('kategori');
+const keluhan = document.getElementById('keluhan');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Format pesan
+    const phoneNumber = '6281263441204'; // nomor ponsel penerima
+    const message = `Kategori: ${kategori.value}%0A Keluhan: ${keluhan.value}`; // isi pesan
+
+    // Buat tautan WhatsApp
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
+
+    // Buka tautan WhatsApp di jendela baru
+    window.open(url);
+
+    // Tampilkan pesan sukses
+    alert('Keluhan berhasil dikirim');
+});
